19c072e8bce571dccb597956b561a69a6cb3b23c
Domains18 <gibsonsgibson88@gmail.com>
2022-11-08 12:05:23 +0300

    file changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import './App.css';
-import Nav from './Frontend/components/Nav/Nav';
-import Header from './Frontend/components/header/Header';
-import Footer from './Frontend/components/Footer/Footer';
 function App() {
   return (
     <>
-      <Nav/>
-      <Header/>
-      <Footer/>
+    <h1>
+      App
+    </h1>
     </>
   );
 }
