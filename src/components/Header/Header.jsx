b4792ba2edecdbe7b7a9cffffe2042b9f585300a
Domains18 <gibsonsgibson88@gmail.com>
2022-11-25 17:23:49 +0300

    deletions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,9 @@
 import React from 'react'
-import Stories from '../../Pages/Stories'
-import Typewriter from 'typewriter-effect'
 import './Header.css'
 const Header = () => {
   return (
     <section>
-        <div className="title">
-            <div className="animation">
-              <div className="typewriter">
-                <Typewriter
-                options={{
-                  strings: [
-                    'Hello', 'We buy and sell', 
-                    'list your property here'
-                  ],
-                  autoStart: true,
-                  loop: true,
-                }}
-                />
-              </div>
-            </div>
-        </div>
-        <Stories/>
+        
     </section>
   )
 }
